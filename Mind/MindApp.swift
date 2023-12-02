2d022797c160462c36b9b7a6ff99416b05e94682
David Wright <DavidWright925@users.noreply.github.com>
2023-12-02 11:08:39 -0800

    Initial Commit

---
+++ Mind/MindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindApp.swift
+//  Mind
+//
+//  Created by David Wright on 2023-12-02.
+//
+
+import SwiftUI
+
+@main
+struct MindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
